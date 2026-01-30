b7141820717281c7cfcae5faa15b9b279cdd6e64
rabbitmaid <rabbitmaid@proton.me>
2026-01-30 20:19:12 +0100

    Added a screen reader only utility class

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ if(form) {
 }
 
 function validateEmail(email) {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailRegex = /^[a-zA-Z0-9_.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
